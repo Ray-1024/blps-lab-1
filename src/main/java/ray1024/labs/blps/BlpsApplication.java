f613bf23282d52496f2fcef2c310f9333408842d
Ray_1024 <skovorodnikovda@mail.ru>
2024-03-29 12:18:08 +0300

    Initial commit

---
+++ src/main/java/ray1024/labs/blps/BlpsApplication.java
@@ -0,0 +1,12 @@
+package ray1024.labs.blps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlpsApplication.class, args);
+	}
+}
