6a918438271ee3a5fe45554fe5b640275e580a3d
Ray_1024 <skovorodnikovda@mail.ru>
2024-04-19 17:19:26 +0300

    Fix PostgreSQL connection and courier repository

--- src/main/java/ray1024/labs/blps/controllers/OrderController.java
+++ src/main/java/ray1024/labs/blps/controllers/OrderController.java
@@ -3,7 +3,6 @@ package ray1024.labs.blps.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import ray1024.labs.blps.model.Order;
 import ray1024.labs.blps.model.responce.UserOrdersResponce;
 
 import java.util.List;
@@ -13,7 +12,7 @@ public class OrderController {
 
     @GetMapping("/api/users/{userId}/orders")
     UserOrdersResponce getOrders(@PathVariable String userId) {
-        return new UserOrdersResponce(List.of(new Order("id",null)));
+        return new UserOrdersResponce(List.of());
     }
 
 }
--- src/main/java/ray1024/labs/blps/model/Courier.java
+++ src/main/java/ray1024/labs/blps/model/Courier.java
@@ -1,4 +1,19 @@
 package ray1024.labs.blps.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Courier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
 }
